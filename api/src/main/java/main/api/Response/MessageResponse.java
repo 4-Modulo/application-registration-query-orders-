6f5eec92c3fbec0114ee2013a2df6b08a7a33c7f
Frederico <frederico.enzo@hotmail.com>
2025-02-28 21:28:54 -0300

    feat: Cria classe MessageResponse para resposta de status

--- api/src/main/java/main/api/Response/MessageResponse.java
+++ api/src/main/java/main/api/Response/MessageResponse.java
@@ -1,2 +1,22 @@
-package main.api.Response;public class MessageResponse {
+package main.api.Response;
+
+import main.api.model.StatusPedido;
+
+public class MessageResponse {
+
+    private String message;
+    private StatusPedido status;
+
+    public MessageResponse(String message, StatusPedido status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
 }
